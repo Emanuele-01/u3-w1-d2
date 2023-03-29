093c8be9825b5e91042a1fac8380d88bbe50c245
Emanuele-01 <emanuele.pitoni.2001@gmail.com>
2023-03-29 17:20:32 +0200

    fine esercizio

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -6,19 +6,23 @@ class AllTheBooks extends Component {
     render() {
     return (
         <Row className="justify-content-center mt-4">
+            <Col xs='10'>
+                <Row>
             {Books.map((param) => (
             <Col xs={3} className="mt-4">
                 <Card className="h-100" key={param.asin}>
                     <Card.Img className="h-50" variant="top" src={param.img} alt={param.title} />
                     <Card.Body className="d-flex flex-column">
-                        <Card.Title>{param.title}</Card.Title>
-                        <Card.Text className="mt-2">{param.category}</Card.Text>
-                        <Card.Text className="mt-2">{param.asin}</Card.Text>
-                        <Button className="mt-auto" variant="danger">{param.price}</Button>
+                        <Card.Title><strong>{param.title}</strong></Card.Title>
+                        <Card.Text className="mt-auto fs-4">{param.category}</Card.Text>
+                        <Card.Text className="mt-auto">{param.asin}</Card.Text>
+                        <Button className="mt-auto" variant="danger">{param.price}€</Button>
                     </Card.Body>
                 </Card>
             </Col>
             ))}
+                </Row>
+            </Col>
         </Row>
     );
 }
